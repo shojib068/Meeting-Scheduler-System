ec06f1acb1ac0081ce38ad399f5cc66fc1aa3de7
AvinasHSinha07 <76842729+AvinasHSinha07@users.noreply.github.com>
2023-02-23 02:05:20 +0600

    user

---
+++ user/navbar.php
@@ -0,0 +1,146 @@
+<?php 
+session_start();
+include "connection.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  
+	<title>
+    Navbar
+	</title>
+
+	  <link rel="stylesheet" type="text/css" href="style.css">
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+<style>
+  /* Navigation bar styles */
+.navbar {
+  background-color: whitesmoke;
+  border: 1px solid #ccc;
+  margin-bottom:0;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  width:100%;
+  height:auto;
+}
+
+.navbar-brand {
+  color: #fff;
+  font-size: 24px;
+  font-weight: bold;
+  display: block;
+  margin: 0 auto;
+  float:left;
+}
+.navbar-header img{
+      width: 150px;
+      height: auto;
+      display: inline-block;
+      padding-bottom: 0px;
+      margin-top:-20px;
+
+    }
+    img {
+  transition: transform 0.3s ease-in-out;
+
+}
+
+img:hover {
+  transform: scale(1.1);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+.navbar-nav{
+  float:right;
+}
+.navbar-nav li{
+  padding:10px;
+}
+.navbar-right{
+  margin-right:0;
+}
+
+.navbar-nav > li > a {
+  color: black;
+  font-size: 18px;
+} 
+
+.navbar-nav > li > a:hover,
+.navbar-nav > li > a:focus {
+  background-color: dodgerblue;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+.glyphicon{
+  /* font-size:20px; */
+  margin-right:5px;
+  vertical-align:middle;
+}
+.container{
+  width: 100%;
+  height:auto;
+}
+
+</style>
+</head>
+<body>
+  <?php 
+$user_email=$_SESSION['login_user'];
+ $sql_app = mysqli_query($db, "SELECT COUNT(status) as total FROM add_meeting where status ='' and attendee_mail = '$user_email';");
+ $a = mysqli_fetch_assoc($sql_app);
+ ?>
+
+	    <nav class="navbar ">
+      <div class="container">
+          <div class="navbar-header">
+          <img src="../images/logo.png">
+            <a class="navbar-brand"></a>
+          </div>
+          <ul class="nav navbar-nav">
+            <li><a href="../index.php">Home</a></li> 
+          </ul>
+<?php 
+if(isset($_SESSION['login_user']))
+{
+  ?>
+   <ul class="nav navbar-nav">
+    <li><a href="profile.php">Profile</a></li>
+   </ul>
+   <ul class="nav navbar-nav">
+    <li><a href="meeting_notification.php"><span class="glyphicon glyphicon-bell style="font-size:2em; margin-left:1000px; margin-top:20px;"></span>
+   <span class="badge ">
+    <?php
+    echo $a['total'];
+    ?>
+   </span></a></li>
+                  <li><a href="profile.php">
+                    <div style="color: black">
+                      <?php
+                        echo "Welcome ".$_SESSION['login_user']; 
+                      ?>
+                    </div>
+                  </a></li>
+                  <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"> Logout</span></a></li>
+                </ul>
+              <?php
+}
+else
+{
+  ?>
+  <ul class="nav navbar-nav">
+  <li><a href="registration.php"><span class="glyphicon glyphicon-user"> </span>Sign-Up</a></li>
+  <li><a href="../login.php"><span class="glyphicon glyphicon-log-in"> </span>Login</a></li>
+ 
+  
+</ul>
+<?php
+}
+?>
+               
+                </div>
+    </nav>
+
+</body>
+</html>
---
+++ user/profile.php
@@ -0,0 +1,173 @@
+<?php 
+	include "connection.php";
+	include "navbar.php";
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Profile</title>
+
+ 	<style>
+.button-container {
+  margin-right: 300px;
+  margin-top:-50px;
+  /* display: inline-block; */
+  vertical-align: center;
+  
+  text-align: center;
+  width: 20%;
+  font-size:100px;
+  float:right;
+
+}
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  width: 360px;
+  margin-left:650px;
+  text-align: center;
+  height:360px;
+  /* display:inline-block; */
+  float:left;
+}
+
+.popover {
+  background-color: #007bff;
+}
+
+.popover-header {
+  color: #fff;
+}
+
+.popover-body {
+  color: #fff;
+}
+
+.btn-default {
+  margin: 10px;
+  font-size:30px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  
+}
+
+.btn-default:hover {
+  transform: scale(1.1);
+}
+
+
+.title {
+  color: grey;
+  font-size: 18px;
+}
+
+button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 200px;
+  font-size: 18px;
+}
+
+.card a {
+  text-decoration: none;
+  font-size: 22px;
+  color: black;
+}
+
+button:hover, a:hover {
+  opacity: 0.7;
+}
+
+.glyphicon glyphicon-bell {
+  font-size: 2em;
+}
+
+h2{
+  margin-top:200px;
+}
+.button-container button{
+	background-color:dodgerblue;
+}
+
+ 	</style>
+ </head>
+
+ <body style="background-color: #F8F8FF; ">
+ 	<div class="container">
+ 		<form action="" method="post">
+ 			<button class="btn btn-secondary" style="float:right; width: 70px;" name="submit1" type="submit">Edit</button>
+ 		</form>
+		 <a type="submit" id="submit" name="submit" href="meeting_notification.php">
+	
+<?php 
+if(isset($_POST['submit']))
+{
+	
+	mysqli_query($db,"UPDATE add_meeting set status='yes' where user_email = '$_SESSION[login_user]';");
+	unset($_SESSION['login_user']);
+}
+?>
+
+ 		<!-- <div class="wrapper"> -->
+ 			<?php
+			
+ 				if(isset($_POST['submit1']))
+ 				{
+ 					?>
+ 						<script type="text/javascript">
+ 							window.location="edit.php"
+ 						</script>
+ 					<?php
+ 				}
+ 				$q=mysqli_query($db,"SELECT * FROM registration where email='$_SESSION[login_user]' ;");
+ 			?>
+ 			<h2 style="text-align: center; ">My Profile</h2>
+
+ 			<?php
+ 				$row=mysqli_fetch_assoc($q);
+				 ?>
+				 <div style="text-align: center;"> <b>Welcome, </b>
+					 <h4>
+						 <?php echo $_SESSION['login_user']; ?>
+					 </h4>
+				 </div>
+				 <div class="card">
+				  <span class="glyphicon glyphicon-user"></span>
+				  <h1><?php echo $row["firstname"]; ?>&nbsp;<?php echo $row['lastname'];?> </h1>
+					 <span class="glyphicon glyphicon-tag"></span>
+				 	<p class="title">User</p> 	
+				 	<span class="glyphicon glyphicon-lock"></span> <P> <?php echo $row['password']; ?></P>	
+				 	<span class="glyphicon glyphicon-map-marker"></span>
+		 
+				 	<p>University of Chittagong</p>
+				 	<p><button>Contact</button></p>
+				
+			  
+  					</span>
+			</div>
+  <div class="button-container text-center">
+					
+
+					<a href="call_meeting.php"><button class="btn btn-default" type="button">Call Meeting</button></a> 
+					
+					 <a href="meeting_list.php"><button class="btn btn-default" type="button">Meeting List</button></a>
+					
+					<a href="edit_agenda.php"><button class="btn btn-default" type="button">Agendas</button></a> 
+					
+								   </div>
+ 
+
+			
+</div>
+
+ 		</div>
+ 	<!-- </div> -->
+	   
+
+</script>
+ </body>
+ </html>
