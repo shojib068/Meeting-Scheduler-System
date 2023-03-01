21baaa06b6f0ff381ff604c47a6ac6b99fe8c509
asif_mahmud <119520020+asifmahmud71@users.noreply.github.com>
2023-03-02 00:14:30 +0600

    Add files via upload

--- user/add_user_to_meeting.php
+++ user/add_user_to_meeting.php
@@ -105,6 +105,7 @@ $meeting_id = $row["id"];
 $sql = "INSERT INTO add_meeting (id,meeting_id, user_mail, attendee_mail,status)
 VALUES ('','$meeting_id', '$user_email', '$attendee_email','')";
 
+
 if (mysqli_query($db, $sql)) {
     echo "Meeting attendee added successfully";
 
@@ -113,8 +114,7 @@ if (mysqli_query($db, $sql)) {
 
 		echo "<table class='table table-bordered table-hover' >";
 			echo "<tr style='background-color: #6db6b9e6;'>";
-				//Table header
-				// echo "<th>"; echo "ID";	echo "</th>";
+				
 				
 				echo "<th>"; echo "Selected ";  echo "</th>";
 			echo "</tr>";	
@@ -122,7 +122,7 @@ if (mysqli_query($db, $sql)) {
 			while($row=mysqli_fetch_assoc($res))
 			{
 				echo "<tr>";
-				// echo "<td>"; echo $row['id']; echo "</td>";
+				
                 echo "<td>"; echo $row['attendee_mail']; 
                 echo "</td>";
 		
@@ -134,6 +134,46 @@ if (mysqli_query($db, $sql)) {
     echo "Error: " . $sql . "<br>" . mysqli_error($db);
 }
 }
+?>
+<?php 
+//email starts
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+require ('../PHPMailer-master/src/Exception.php');
+require '../PHPMailer-master/src/PHPMailer.php';
+require ('../PHPMailer-master/src/SMTP.php');
+if(isset($_POST['add_to_meeting']))
+{
+    $mail = new PHPMailer(true);
+    try{
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail -> isSMTP();
+    $mail -> Host = 'smtp.gmail.com';
+    $mail -> SMTPAuth = true;
+    $mail -> Username = 'kawsarshojib068@gmail.com';
+    $mail ->Password = 'gxgmnszcqpqswnsr';
+    $mail->SMTPSecure="tls";
+    $mail ->Port = 587;
+    $mail ->setFrom('kawsarshojib068@gmail.com');
+    $mail ->addAddress('asifmahmudmeraj2016@gmail.com');
+    $mail ->isHTML(true);
+    $mail ->Subject ="Always same";
+    $mail ->Body = 'No body';
+    $mail -> send();
+    }catch (Exception $e){
+        echo "Message could not be sent. Mail Error:{$mail->ErrorInfo}";
+    }
+    
+
+
+}
+
+
+
+
+
+//email ends
 ?>
 
 </body>
